57ae775597c7a9061f4723c6b03606d5d7c1b953
Yzabelle-M <s2023101768@firstasia.edu.ph>
2024-10-18 19:21:40 +0800

    Add files via upload

---
+++ Model/ReaderModels.cs
@@ -0,0 +1,14 @@
+﻿namespace Models
+{
+    public class ReaderModel
+    {
+        public class Reader
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string UserName { get; set; }
+        }
+
+    }
+}
